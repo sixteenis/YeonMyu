42e074c3e8c59ce8fec84dfe90e4395e16b9579a
sixteenis <psm0103907@naver.com>
2025-04-02 22:06:13 +0900

    상세뷰 스크롤 구현

--- YeonMyu/Presentatiton/Screens/PlayDetail/PlayDetailView.swift
+++ YeonMyu/Presentatiton/Screens/PlayDetail/PlayDetailView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 enum ScrollType: Int {
     case playInfo
@@ -16,61 +17,103 @@ enum ScrollType: Int {
 struct PlayDetailView: View {
     var postID: String
     var exUrl = "http://www.kopis.or.kr/upload/pfmPoster/PF_PF248955_240912_102352.gif"
+    var exInfoUrl = ["http://www.kopis.or.kr/upload/pfmIntroImage/PF_PF245617_240723_0146290.jpg"]
     let segments: [String] = ["공연정보", "티켓예매", "위치/시설"]
-    let model = DetailPerformance()
+    var data = DetailPerformance()
     @Namespace private var name
     @State private var currentPage = 0
     @State private var selectPage = 0
-    @State private var position = ScrollPosition(edge: .top) // idType을 Int.self로 수정
+    @State private var position = ScrollPosition(edge: .top)
     @State private var isStopCurrentPage = false
+    @State private var allInfo = false
     
     var body: some View {
-        //NavigationStack {
-            ScrollView {
-                LazyVStack(pinnedViews: [.sectionHeaders]) {
-                    postView()
-                        .padding(.bottom, 40)
-                    Section(header: stickyHeader()) {
-                        playInfo()
-                            .frame(height: 600)
-                            .id(0) // Int 타입 ID
-                        
-                        ticketInfo()
-                            .frame(height: 600)
-                            .id(1) // Int 타입 ID
-                        
-                        placeInfo()
-                            .frame(height: 900)
-                            .id(2) // Int 타입 ID
-                    }
+        ScrollView {
+            LazyVStack(spacing: 0, pinnedViews: [.sectionHeaders]) {
+                postView()
+                    .padding(.bottom, 40)
+                    .id(-1)
+                Color.clear.frame(height: 1).id(0)
+                Section(header: stickyHeader()) {
+                    playInfo()
+                        .id(1)
+                    inforPost()
+                        .id(2)
+                    Rectangle()
+                        .frame(height: 1)
+                        .id(3)
                     
-                } //: LazyVStack
-                .scrollTargetLayout()
-            } //: ScrollView
-            .navigationTitle("공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명")
-            .scrollPosition($position, anchor: .top)
-            .onScrollTargetVisibilityChange(idType: Int.self) { id in
-                guard let firstId = id.first, !isStopCurrentPage else { return }
-                print("스크롤 감지로 변경 \(firstId)")
-                currentPage = firstId
-                selectPage = 4 //의미없는 값으로 사용자 탭 값 변경해주기
-            }
-            .onChange(of: selectPage) { oldValue, newValue in
-                print("사용자 클릭 감지로 변경 \(newValue)")
-                if selectPage == 4 { return } //의미없는 값일경우 로직 동작 X
-                self.isStopCurrentPage = true
-                withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.3)) {
-                    position.scrollTo(id: newValue)
-                    currentPage = selectPage
-                } completion: {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                        print("완료!!!")
-                        self.isStopCurrentPage = false
-                    }
+                    Rectangle()
+                        .frame(height: 50)
+                    
+                    
+                    
+                    Rectangle()
+                        .frame(height: 1)
+                        .id(4)
+                    ticketInfo()
+                        .frame(height: 600)
+                        .id(5)
+                    Rectangle()
+                        .frame(height: 12)
+                        .id(6)
+                    Rectangle()
+                        .frame(height: 12)
+                        .id(7)
+                    placeInfo()
+                        .frame(height: 600)
+                        .id(8)
+                    Rectangle()
+                        .frame(height: 160)
+                        .id(9)
                 }
             }
+            .scrollTargetLayout()
+        }
+        .navigationTitle("공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명공연명")
+        .scrollPosition($position, anchor: .top)
+        .onScrollTargetVisibilityChange(idType: Int.self) { id in
+            // 사용자가 클릭으로 스크롤 중일 때는 동작하지 않음
+            guard !isStopCurrentPage, let firstId = id.first else { return }
             
-      //  } //: NavigationStack
+            print("스크롤 감지로 변경 \(firstId)")
+            let newcurrent: Int
+            switch firstId {
+            case -1,0,1,2,3:
+                newcurrent = 0
+                selectPage = -1
+            case 4,5,6: newcurrent = 1
+            case 7,8,9: newcurrent = 2
+            default: newcurrent = 0
+            }
+            withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.3)) {
+                currentPage = newcurrent
+            }
+        }
+        .onChange(of: selectPage) { oldValue, newValue in
+            print("사용자 클릭 감지로 변경 이전 \(oldValue)")
+            print("사용자 클릭 감지로 변경 \(newValue)")
+            isStopCurrentPage = true
+            
+            
+            withAnimation(.interactiveSpring(response: 0.5, dampingFraction: 0.8, blendDuration: 0.3)) {
+                switch newValue {
+                case 0:
+                    position.scrollTo(id: 0, anchor: .top)
+                    currentPage = newValue
+                case 1:
+                    position.scrollTo(id: 5, anchor: .top)
+                    currentPage = newValue
+                case 2:
+                    position.scrollTo(id: 8, anchor: .top)
+                    currentPage = newValue
+                default: break
+                }
+            } completion: {
+                print("스크롤 완료!!!")
+                isStopCurrentPage = false
+            }
+        }
     }
 }
 
@@ -122,10 +165,9 @@ private extension PlayDetailView {
                 HStack(spacing: 0) {
                     ForEach(segments.indices, id: \.self) { index in
                         Button {
-                            withAnimation {
+//                            withAnimation {
                                 selectPage = index
-                                currentPage = index
-                            }
+//                            }
                             
                         } label: {
                             ZStack {
@@ -138,14 +180,14 @@ private extension PlayDetailView {
                                     .foregroundStyle(Color.asBorderGrayLine)
                                     .vBottom()
                             }
-                                
+                            
                         }
                     }
                 }
                 Rectangle()
                     .frame(width: UIScreen.main.bounds.width / CGFloat(segments.count), height: 4)
                     .foregroundStyle(Color.asPurple300)
-                    .animation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0.2), value: currentPage)
+//                    .animation(.spring(response: 0.4, dampingFraction: 0.8, blendDuration: 0.2), value: currentPage)
                     .offset(x: CGFloat(currentPage) * (UIScreen.main.bounds.width / CGFloat(segments.count)))
                     .vBottom()
             }
@@ -153,34 +195,67 @@ private extension PlayDetailView {
         }
         .background(Color.asWhite)
     }
+    //공연정보
     func playInfo() -> some View {
-        VStack(spacing: 14) {
+        VStack(alignment: .leading, spacing: 28) {
             asText("공연 정보")
                 .font(.font20)
                 .foregroundStyle(Color.asFont)
+                .padding([.horizontal, .top], 24)
+            customInfo(header: "공연기간", info: "임시 텍스트!!!!!!!!!!!")
+            customInfo(header: "공연장소", info: "임시 텍스트!!!!!!!!!!!")
+            customInfo(header: "공연시간", info: "임시 텍스트!!!!!!!!!!!")
+            customInfo(header: "러닝타임", info: "임시 텍스트!!!!!!!!!!!")
+            customInfo(header: "관람연령", info: "임시 텍스트!!!!!!!!!!!임시 텍스트!!!!!!!!!!!임시 텍스트!!!!!!!!!!!임시 텍스트!!!!!!!!!!!임시 텍스트!!!!!!!!!!!")
+            customInfo(header: "출연배우", info: "임시 텍스트!!!!!!!!!!!")
+            customInfo(header: "제작진", info: "임시 텍스트!!!!!!!!!!!")
         }
     }
-    
     func customInfo(header: String, info: String) -> some View {
-        HStack(alignment: .top, spacing: 46) {
+        HStack(alignment: .top, spacing: 0) {
             asText(header)
                 .font(.font16)
                 .foregroundStyle(Color.asFont)
+                .frame(width: 100, alignment: .leading)
             asText(info)
                 .font(.font16)
                 .foregroundStyle(Color.asGray100)
-                .multilineTextAlignment(.leading) // 줄바꿈시 정렬이 적용안되는 이슈 해결용!
+                //.multilineTextAlignment(.leading) // 줄바꿈시 정렬이 적용안되는 이슈 해결용!
         }
+        .padding(.horizontal, 24)
     }
-    
-    
-    
     func ticketInfo() -> some View {
         Text("티켓 정보")
     }
     func placeInfo() -> some View {
         Text("공연장 정보")
     }
+}
+//공연 설명 포스터 부분
+private extension PlayDetailView {
+    func inforPost() -> some View {
+        VStack(spacing: 0) {
+            Rectangle()
+                .frame(height: allInfo ? 2000 : 300)
+            Button {
+                // Action
+                allInfo.toggle()
+            } label: {
+                Text("전체보기")
+            }
+            //ForEach(data.DetailPosts, id: \.self) {
+//            ForEach(exInfoUrl, id: \.self) {
+//                KFImage(URL(string: $0))
+//                    .placeholder { //플레이스 홀더 설정
+//                        Image.postPlaceholder
+//                            .resizable()
+//                    }.retry(maxCount: 3, interval: .seconds(5)) //재시도
+//                    .resizable()
+//                    .aspectRatio(contentMode: .fill)
+//            }
+        }
+    }
+
 }
 
 #Preview {
